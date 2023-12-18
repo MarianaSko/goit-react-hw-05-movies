681f6ba6fb8da75a3312790faa2a8d286aed1972
MarianaSko <maryana.skorynovych@gmail.com>
2023-12-18 16:42:01 +0200

    v-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,18 +10,16 @@ const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
-    <Suspense fallback={<h1>Loading</h1>}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:id" element={<MovieInfoPage />}>
-            <Route path="cast" element={<Cast />}></Route>
-            <Route path="reviews" element={<Reviews />}></Route>
-          </Route>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:id" element={<MovieInfoPage />}>
+          <Route path="cast" element={<Cast />}></Route>
+          <Route path="reviews" element={<Reviews />}></Route>
         </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Suspense>
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
